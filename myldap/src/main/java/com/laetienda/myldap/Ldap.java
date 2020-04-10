f6315eb9c758ff51010572aae1f288984fa7f2ca
I849921 <I849921@YMQN34401276A.amer.global.corp.sap>
2020-04-10 19:29:31 -0400

    Saving everything into GitHyub to be imported on MAC workstation

--- myldap/src/main/java/com/laetienda/myldap/Ldap.java
+++ myldap/src/main/java/com/laetienda/myldap/Ldap.java
@@ -1,16 +1,12 @@
 package com.laetienda.myldap;
 
-//import java.io.IOException;
 import java.util.Iterator;
 
-//import org.apache.directory.api.ldap.model.cursor.CursorException;
-//import org.apache.directory.api.ldap.model.cursor.EntryCursor;
 import org.apache.directory.api.ldap.model.entry.Attribute;
 import org.apache.directory.api.ldap.model.entry.Entry;
 import org.apache.directory.api.ldap.model.entry.Modification;
 import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
-//import org.apache.directory.api.ldap.model.message.SearchScope;
 import org.apache.directory.api.ldap.model.name.Dn;
 import org.apache.directory.ldap.client.api.LdapConnection;
 import org.apache.logging.log4j.LogManager;
@@ -36,62 +32,6 @@ public class Ldap {
 		
 		return result;
 	}
-	
-	/**
-	 * @deprecated Better to use LdapConnection.lookup()
-	 * @param dnstr
-	 * @param conn
-	 * @return
-	 * @throws Exception
-	 */
-//	public Entry searchDn(String dnstr, LdapConnection conn) throws Exception{
-//		
-//		Entry result = null;
-//		
-//		try {
-//			Dn dn = new Dn(dnstr);
-//			result = searchDn(dn,conn);
-//			
-//		} catch (Exception e) {
-//			throw e;
-//		}
-//		
-//		return result;
-//	}
-	
-	/**
-	 * @deprecated Better use LdapConnection.lookup(Dn)
-	 * @param dn
-	 * @param conn
-	 * @return
-	 * @throws Exception
-	 */
-//	public Entry searchDn(Dn dn, LdapConnection conn) throws Exception {
-//		log.info("Searching for dn entry in LDAP. $dn: {} ...", dn.getName());
-//		Entry result = null;
-//		EntryCursor cursor = null;;
-//		try {
-//			cursor = conn.search(dn, "(objectclass=*)",  SearchScope.OBJECT);
-//			
-//			if(cursor.next()) {
-//				result = cursor.get();
-//			}
-//			
-//			if(cursor.iterator().hasNext()) {
-//				result = null;
-//				log.warn("dn: %s exists more than once", dn);
-//			}
-//			
-//			log.info("... sarch for dn has finished");
-//		} catch (LdapException  | CursorException e) {
-//			log.warn("Failed to search dn entry.");
-//			throw e;
-//		} finally {
-//			closeCursor(cursor);
-//		}
-//		
-//		return result;
-//	}
 	
 	/**
 	 * @param ldapEntity
@@ -169,14 +109,6 @@ public class Ldap {
 			if(ldapEnity.getErrors().size() > 0) {
 				log.warn("Failed to modify LdapEntity due to invalid user input");
 			}else {
-				
-//				Iterator<Modification> iter = ldapEnity.getModifications().iterator();
-//				
-//				while(iter.hasNext()) {
-//					Modification temp = iter.next();
-//					conn.modify(ldapEnity.getLdapEntry().getDn(), temp);
-//					ldapEnity.getModifications().remove(temp);
-//				}
 				
 				for(Modification modification : ldapEnity.getModifications()) {
 					conn.modify(ldapEnity.getLdapEntry().getDn(), modification);
